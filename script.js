269ca7712eeb81834f0985b7257893c4946c4141
Yaseen Al Hasan <ya705668@gmail.com>
2023-09-01 10:29:14 -0700

    nav complet

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 
 
 const postCategory = async () => {
-    const response = await fetch("https://openapi.programming-hero.com/api/videos/category/1000#downloadJSON=true")
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await response.json();
-    console.log(data)
+
+    const categoryList = document.getElementById('category');
+
+    const dataInfo = data.data
+
+    dataInfo.forEach((category) => {
+
+        // console.log(category.category_id)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a onclick="categoryType('${category.category_id}')" class="tab tab-bordered">${category.category}</a>
+        `;
+        categoryList.appendChild(div)
+    });
+    // console.log(dataInfo)
 }
+
+const categoryType = async (category_id) => {
+    //console.log(category_id)
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
+    const data = await response.json();
+    console.log(data.data)
+}
+
 postCategory()
