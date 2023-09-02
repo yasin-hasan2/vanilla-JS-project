321eb8afc6b37a9e059464c8506a8c7dd033e348
Yaseen Al Hasan <ya705668@gmail.com>
2023-09-01 22:27:37 -0700

    helf work completed

--- script.js
+++ script.js
@@ -24,7 +24,44 @@ const categoryType = async (category_id) => {
     //console.log(category_id)
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
     const data = await response.json();
-    console.log(data.data)
+    //console.log(data.data)
+
+    const cardContainer = document.getElementById('card_container')
+
+    data.data.forEach((videos) => {
+        console.log(videos)
+        const div = document.createElement('div')
+        div.innerHTML = `
+                        <div class="card w-96 bg-base-100 shadow-xl">
+                    <figure>
+                        <img src=${videos?.thumbnail} alt="Shoes" />
+                    </figure>
+                    <div class="card-body">
+                        <div class="card-footer">
+                            <div class="flex">
+                                <div>
+                                    <div class="avatar online">
+                                        <div class="w-8 rounded-full">
+                                            <img
+                                                src=${videos?.authors[0]?.profile_picture} />
+                                        </div>
+                                    </div>
+                                </div>
+                                <h3 class="text-base font-bold pl-3"> ${videos?.title}
+                                </h3>
+
+                            </div>
+                            <div class="mt-4">
+                                <h6 class="text-sm font-normal text-[#171717B2]">${videos?.authors[0]?.profile_name}</h6> <a href="">${videos?.authors[0]?.verified}</a>
+
+                                <small>views:${videos?.others?.views}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `
+        cardContainer.appendChild(div)
+    })
 }
 
 postCategory()
